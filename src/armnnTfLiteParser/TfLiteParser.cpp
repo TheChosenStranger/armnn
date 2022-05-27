1a745a53a94f070dcdbd985bdb179b79ffe5641a
TheChosenStranger <moustafa.ghareeb.hpg@gmail.com>
2022-05-27 14:36:09 +0200

    [Bug fix] ExpandDims Segmentation fault ExpandDims layer in TFLite caused segmentation fault while being parsed, this was fixed in the code.

--- src/armnnTfLiteParser/TfLiteParser.cpp
+++ src/armnnTfLiteParser/TfLiteParser.cpp
@@ -1,4 +1,4 @@
-//
+﻿//
 // Copyright © 2017 Arm Ltd and Contributors. All rights reserved.
 // SPDX-License-Identifier: MIT
 //
@@ -1362,7 +1362,7 @@ void TfLiteParserImpl::ParseExpandDims(size_t subgraphIndex, size_t operatorInde
     }
     else
     {
-        int32_t axis = inputs[1]->shape[0];
+        int32_t axis = inputs[0]->shape[0];
 
         int32_t inputDimSize = static_cast<int32_t>(inputTensorInfo.GetShape().GetNumDimensions());
 
